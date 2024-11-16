bb102975ed46e04e027a3bfb51dd5b3568c3e85f
Nicoo73 <nicolas.molina04@alumnos.uach.cl>
2024-11-15 21:44:42 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,36 +1,52 @@
-import PendingReturns from './components/PendingReturns'
-import Statistics from './components/Statistics'
-import MostRequestedBooks from './components/MostRequestedBooks'
-import TopReaders from './components/TopReaders'
-import TopDonors from './components/TopDonors'
-import LinesChart from './components/LineChart'
-//import LinesChart from './components/LineChart'
+// page.js
+import PendingReturns from "./components/PendingReturns";
+import Statistics from "./components/Statistics";
+import MostRequestedBooks from "./components/MostRequestedBooks";
+import TopReaders from "./components/TopReaders";
+import TopDonors from "./components/TopDonors";
+import LinesChart from "./components/LineChart";
 
+// Esta función se ejecutará en el servidor
+export async function getData() {
+  try {
+    const response = await fetch("https://dlp-api.vercel.app/prestamos", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
 
-export default function Dashboard() {
+    if (!response.ok) {
+      throw new Error("Error al obtener los datos");
+    }
+
+    const res = await response.json();
+    return res.prestamos.length;
+  } catch (e) {
+    console.error("Error al obtener los datos:", e);
+    return 0;
+  }
+}
+
+export default async function DashboardPage() {
+  const totalBorrowedBooks = await getData();
+
   return (
     <div className="dashboard grid grid-cols-4 gap-4">
-      <div className="col-span-3 ">
-       
+      <div className="col-span-3">
         <PendingReturns />
         <LinesChart />
-        
-       
       </div>
       <div className="col-span-1">
-        <Statistics />
-        
-        
+        <Statistics totalBorrowedBooks={totalBorrowedBooks} />
       </div>
-      <div className="col-span-3 ">
-        <MostRequestedBooks />  
+      <div className="col-span-3">
+        <MostRequestedBooks />
       </div>
-      
       <div className="col-span-1 space-y-4">
         <TopReaders />
         <TopDonors />
       </div>
-      
     </div>
-  )
+  );
 }
